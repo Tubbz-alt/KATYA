518427ad40c33522a344da2969ceec6978599bf8
shawn25n92 <shawn25n92@gmail.com>
2017-11-15 12:35:30 +0800

    Malaysian Related Algorithms

---
+++ KATYA/KATYAMalaysia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KATYA
+{
+    public class KATYAMalaysia
+    {
+        public Dictionary<string, string> NRICPlacesOfBirth = new Dictionary<string, string>()
+        {
+
+        };
+        public KATYAMalaysia()
+        {
+
+        }
+        public StatusObject GenerateNRICNumbers()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+    }
+}
