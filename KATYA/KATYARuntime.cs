07a5e262fbafed036521561a492c24551a057d18
shawn25n92 <shawn25n92@gmail.com>
2017-11-15 18:03:27 +0800

    Installation Directories Test
    This will create directories at the same level as the .exe

--- KATYA/KATYARuntime.cs
+++ KATYA/KATYARuntime.cs
@@ -4,27 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
-
+using System.IO;
 namespace KATYA
 {
     public class KATYARuntime
     {
-        public Dictionary<string, string> InstallDirectories = new Dictionary<string, string>()
-        {
-            // Main Directory
-            {"Main",@"{0}KATYA"}
-        };
+        public Dictionary<string, string> InstallDirectories { get; set; }
         public KATYARuntime()
         {
-
+            try
+            {
+                /*
+                 Template: InstallDirectories.Add("", "");
+                 */
+                InstallDirectories = new Dictionary<string, string>();
+                InstallDirectories.Add("Main", "KATYA");
+                InstallDirectories.Add("KATYACryptographyMain", @"KATYA\Cryptography");
+                InstallDirectories.Add("KATYADatabaseMain", @"KATYA\Database");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
         public StatusObject BuildDirectories()
         {
             StatusObject SO = new StatusObject();
             try
             {
-                Assembly KATYA = Assembly.GetExecutingAssembly();
-                Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+                foreach(KeyValuePair<string,string> InstallDirectory in InstallDirectories)
+                {
+                    Directory.CreateDirectory(InstallDirectory.Value);
+                }
+                
             }
             catch(Exception e)
             {
