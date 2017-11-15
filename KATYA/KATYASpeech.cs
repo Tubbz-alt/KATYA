1e9f317329831ced800f9944664cc3057223a589
shawn25n92 <shawn25n92@gmail.com>
2017-11-15 10:43:00 +0800

    Primitive Speech Component

---
+++ KATYA/KATYASpeech.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+namespace KATYA
+{
+    public class KATYASpeech
+    {
+        public KATYASpeech()
+        {
+
+        }
+        public StatusObject Speak()
+        {
+            StatusObject SO = new StatusObject();
+            return SO;
+        }
+        public StatusObject Listen()
+        {
+            StatusObject SO = new StatusObject();
+            return SO;
+        }
+        public StatusObject ProcessInstruction()
+        {
+            StatusObject SO = new StatusObject();
+            return SO;
+        }
+        public StatusObject RecordInstruction()
+        {
+            StatusObject SO = new StatusObject();
+            return SO;
+        }
+    }
+}
