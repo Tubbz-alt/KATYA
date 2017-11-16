882e6f539c075a9f7b1da531215230cb6d31d378
shawn25n92 <shawn25n92@gmail.com>
2017-11-16 17:31:43 +0800

    Web Utilities
    For generating web requests

---
+++ KATYA/KATYAWeb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KATYA
+{
+    public partial class KATYAWeb
+    {
+        public string URL { get; set; }
+        public KATYAWeb()
+        {
+
+        }
+        public KATYAWeb(string URL)
+        {
+            this.URL = URL;
+        }
+        public StatusObject HTTPGet()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+        public StatusObject HTTPPost()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+    }
+}
