3ba723a2c53b8eb3ad5526bccd35e680534c050b
shawn25n92 <shawn25n92@gmail.com>
2017-11-20 17:13:28 +0800

    Just in case

---
+++ KATYA/KATYAMerimen.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KATYA
+{
+    class KATYAMerimen
+    {
+        /*
+            sleEPLPOLID	
+            slePOLID	
+            sleCURINSCOID	0
+            ddlbINSCOID	4
+            ddlbInsurer	4
+            ddlbCLAIMTYPE	OD
+            flg_epl_integration	0
+            flg_epl_integration_pol_found	0
+            cbxSUBCLMTYPE	0
+            CLMFLAGMASK	4
+            CLMFLAG	0
+            INS_NEXT_CSTAT	1
+            sleACCDATE	20/11/2017
+            sleACCTIME	00
+            sleCLMnmsel	2
+            sleCLMnm	POSTTEST
+            REGCMTINJUREDEXISTS	1
+            REGCMTINJURED	1
+            sleCLMID1sel	3
+            sleCLMID1	POSTTEST
+            sleCLMID2sel	4
+            sleCLMID2	
+            sleREGNO	POSTTEST
+            sleCHANO	
+            ddlbREPCOID	1137
+            NOWORKSHOP	1
+            REPTRFCASEEXISTS	8
+            sleINITIALEST_DD	50000~40,000+-+50,000
+            sleINITIALEST_SLE	50,000
+            sleINITIALEST_SEL	DD
+            SETTLETYPE	0
+            ddlbDAMTYPE	
+            mleLossDesc	
+            sibackend	1
+            slePOLNO	POSTTEST
+            siISCOVERNOTEOPTION	1
+            ddlbPOLCOVER	1
+            ddlbNEWPOL	
+            LBLJS	30781,30952
+            slePOLFROM	20/11/2017
+            slePOLTO	20/11/2017
+            mnSUMINSURED	
+            mnSUMINSURED2	
+            preexclist	
+            preexcxml	
+            intexclist	
+            intexcxml	
+            othexcxml	
+            othexcremlist	
+            savetot	
+            DXS	50,000.00
+            DXSWAIVEDEXISTS	1
+            mn2F	
+            sle2FREMARKS	
+            sleInmsel	2
+            sleInm	
+            sleIID1sel	2
+            sleIID1	
+            sleIID2sel	4
+            sleIID2	
+            sleTPREGNO	
+            ddlbKFK	
+            TPINSCOID_EXISTS	1
+            PSCID	4
+            ddlbMan	10
+            ddlbModel	5827
+            FORMGUID	7643AD19-CE08-E226-8C8B-F2AEB352D8C8
+         
+         */
+    }
+}
